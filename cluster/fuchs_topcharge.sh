5591cb8508f86ed38d1d96362a069e66ab79006c
atemynany <atemybaby12@gmail.com>
2026-04-05 21:46:38 +0200

    topcharge logs

--- cluster/fuchs_topcharge.sh
+++ cluster/fuchs_topcharge.sh
@@ -8,9 +8,10 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=0
 #SBATCH --time=24:00:00
-#SBATCH --output=topcharge_%j.out
-#SBATCH --error=topcharge_%j.err
+#SBATCH --output=logs/topcharge_%j.out
+#SBATCH --error=logs/topcharge_%j.err
 
 cd /work/mesonqcd/barros/SU23_freezing/Topological-freezing-in-SU2-3-simulations
+mkdir -p logs
 
 bash run_topcharge_scan.sh --su2 --skip-build --jobs 20
