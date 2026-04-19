c43fe400bf2495829dd9a8139c913444b4442d8e
atemynany <atemybaby12@gmail.com>
2026-04-19 15:36:26 +0200

    add SU3 topcharge test and related updates

--- cluster/hhlr_heatbath.sh
+++ cluster/hhlr_heatbath.sh
@@ -13,9 +13,12 @@
 # Purge the environment to prevent dynamic linking conflicts
 module purge
 
-# Set working directory
-cd /work/mesonqcd/barros/SU23_freezing/Topological-freezing-in-SU2-3-simulations
-mkdir -p logs
+# Output/results go to work; binary and script run from home
+export HEATBATH_RESULTS_DIR=/work/mesonqcd/barros/SU23_freezing/Topological-freezing-in-SU2-3-simulations/data/results
+mkdir -p /work/mesonqcd/barros/SU23_freezing/Topological-freezing-in-SU2-3-simulations/logs
+
+# Set working directory to home (binary lives here)
+cd /home/mesonqcd/barros/SU_Calc/Topological-freezing-in-SU2-3-simulations
 
 # Explicitly map OpenMP threads to the requested physical cores
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
